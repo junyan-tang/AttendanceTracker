52e3ae79a3545d3771414a9665f4c68f5254ab54
Tono-vacn <yuchen.jiang@duke.edu>
2024-03-20 11:55:27 -0400

    lecture added

--- gradle-skeleton/server/src/main/java/edu/duke/ece651/team4/server/Lecture.java
+++ gradle-skeleton/server/src/main/java/edu/duke/ece651/team4/server/Lecture.java
@@ -1,6 +1,9 @@
 package edu.duke.ece651.team4.server;
 
 import edu.duke.ece651.team4.server.Course;
+import edu.duke.ece651.team4.server.User;
+import edu.duke.ece651.team4.server.Attendance;
+import edu.duke.ece651.team4.server.MessageSender;
 import java.util.LinkedHashMap;
 
 public class Lecture implements Course{
@@ -8,6 +11,9 @@ public class Lecture implements Course{
   private String courseName;
   private LinkedHashMap<String, User> studentMap;
   private LinkedHashMap<String, User> professorMap;
+  private LinkedHashMap<String, Attendance> attendanceMap;
+  private MessageSender messageSender;
+
   
 
 }
---
+++ gradle-skeleton/server/src/main/java/edu/duke/ece651/team4/server/Professor.java
@@ -0,0 +1,5 @@
+package edu.duke.ece651.team4.server;
+
+public class Professor {
+  
+}
