351e7617b6f518cc0ddf1c55e9c170e14a6ac5ad
Yuhan <yx167@duke.edu>
2024-03-21 20:57:47 -0400

    add input filter class

---
+++ gradle-skeleton/server/src/main/java/edu/duke/ece651/team4/server/InputFilter.java
@@ -0,0 +1,10 @@
+package edu.duke.ece651.team4.server;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+
+public class InputFilter {
+  private final BufferedReader inputReader;
+
+}
