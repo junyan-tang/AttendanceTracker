583d0fb9cafb9e005c597e3073fa32fde1fca130
junyan-tang <junyantang1008@gmail.com>
2024-03-23 19:33:50 -0400

    add SHA-256 to hash password

--- gradle-skeleton/server/src/main/java/edu/duke/ece651/team4/server/SecurityManager.java
+++ gradle-skeleton/server/src/main/java/edu/duke/ece651/team4/server/SecurityManager.java
@@ -1,8 +1,21 @@
 package edu.duke.ece651.team4.server;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public abstract class SecurityManager {
     public abstract Boolean checkUserExist(String netid);
     public String hashPassword(String password){
-        return password;
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+            StringBuilder hashedpwd = new StringBuilder();
+            for (int i = 0; i < digest.length; i++) {
+                hashedpwd.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return hashedpwd.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return password;
+        }
     }
 }
