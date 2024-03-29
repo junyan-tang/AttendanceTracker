519be33da6f2899b65db40c5eb842bc9ed73d269
junyan-tang <junyantang1008@gmail.com>
2024-03-28 22:16:51 -0400

    initialize a new project

---
+++ src/main/java/edu/duke/ece651/team4/AttendanceTrackerApp/AttendanceTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.team4.AttendanceTrackerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/duke/ece651/team4/AttendanceTrackerApp/AttendanceTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.team4.AttendanceTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
